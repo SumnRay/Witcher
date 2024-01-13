44da350fbeae3b541b8bf3dc50f247c6751df720
ILya <tkachduty@gmail.com>
2024-01-13 17:02:57 +0300

    СОЗДАНИЕ МОДЕЛИ ДАННЫХ ПРИЛОЖЕНИЯ

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,10 +1,8 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/test')
-var schema = mongoose.Schema({ name: String })
-schema.methods.obmen = function(){
-console.log(this.get("name") + " Сказал 'Здравствуй'")
-}
-var Wit = mongoose.model('Wit', schema)
-
-var Wit = new Wit({ name: 'Геральт' });
-Wit.save().then(() => Wit.obmen());
+mongoose.connect('mongodb://localhost/test1')
+var Witch = require("./models/witch").Witch
+var witch = new Witch({
+title: "Геральт",
+})
+console.log(witch)
+witch.save()
