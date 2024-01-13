69fec17e228e280cd8fa49cde7dbf8e5a2da5381
ILya <tkachduty@gmail.com>
2024-01-13 16:41:33 +0300

    data

---
+++ data.js
@@ -0,0 +1,21 @@
+var data = [{
+    title: 'Геральт',
+    nick: 'geralt',
+    avatar: '/images/geralt.jpg',
+    desc: 'Главный герой произведения, ведьмак школы волка, охотник на чудовищ.'
+    },
+    {
+    title: 'Йеннифер',
+    nick: 'yennifer',
+    avatar: '/images/yennifer.jpg',
+    desc: 'Чародейка, супруга геральта.'
+    },
+    {
+    title: 'Цири',
+    nick: 'ciri',
+    avatar: '/images/ciri.jpg',
+    desc: 'Приемная дочь геральта, ведьмачка.'
+    }
+    ];
+    module.exports.data = data;
+    
