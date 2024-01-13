25472dab11d058c004008a5b1d39a92a5a858a30
ILya <tkachduty@gmail.com>
2024-01-13 17:10:53 +0300

    ОБРАБОТКА ПАРАМЕТРА В АДРЕСЕ

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ mongoose.connect('mongodb://localhost/witcher')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var witchRouter = require('./routes/witch');
 
 var app = express();
 
@@ -24,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/witch', witchRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
