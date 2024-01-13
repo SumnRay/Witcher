2b12c143b80e88b88778c071fa5269964e15fb19
ILya <tkachduty@gmail.com>
2024-01-13 16:23:19 +0300

    МАРШРУТИЗАТОР

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,14 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/geralt', function(req, res, next) {
+  res.send("<h1>Страница Геральта</h1>")
+  });
+  router.get('/yennifer', function(req, res, next) {
+    res.send("<h1>Страница Йеннифер</h1>")
+    });
+    router.get('/ciri', function(req, res, next) {
+      res.send("<h1>Страница Цири</h1>")
+      });
 
 module.exports = router;
