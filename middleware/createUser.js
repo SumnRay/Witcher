bbb60f8991be7c54e6e03d6f1378134ab99b18ef
ILya <tkachduty@gmail.com>
2024-01-14 21:56:01 +0300

    ИНФОРМАЦИЯ О ПОЛЬЗОВАТЕЛЕ

--- middleware/createUser.js
+++ middleware/createUser.js
@@ -1,7 +1,20 @@
-const User = require("../models/user").User;
+// const User = require("../models/user").User;
+var db = require('../mySQLConnect.js');
 
 module.exports = async function(req, res, next) {
   res.locals.user = null
+  console.log('Проверяем сессию')
+  console.log(req.session.user)
+  db.query(`SELECT * FROM user WHERE user.id = '${req.session.user}'`, (err,
+  users) => {
+  if(err)
+  return next(err)
+  if(users.length > 0) {
+  res.locals.user = users[0];
+  }
+  next()
+  })
+  
   // try {
   //   const user = await User.findById(req.session.user).exec();
   //   if (!user) {
@@ -12,6 +25,5 @@ module.exports = async function(req, res, next) {
   //   next();
   // } catch (err) {
   //   next(err);
-  // }
-  next();
-};
+  // 
+}
