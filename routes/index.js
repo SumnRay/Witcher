e7c4a7321abd2ee2bbd0eff437d57ade2e644a0f
ILya <tkachduty@gmail.com>
2024-01-14 17:33:45 +0300

    КОМАНДА ЗАПИСИ В COOKIE

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 const Witche = require("../models/witche").Witche
 
-
 /* GET home page. */
 router.get('/', async (req, res, next) => {
   try {
+    router.get('/', function(req, res, next) {
+      res.cookie('greeting', 'Hi!!!').render('index', { title: 'Express', menu:menu });
+      });
     const menu = await Witche.find({}, { _id: 0, title: 1, nick: 1 });
     res.render('index', {
       title: 'Witchery',
